5cf26cf5e2fe783a8cef392f350fd577824f7e13
Christoph Doblander <chris@degree.at>
2009-10-16 21:43:36 +0200

    initial api concept

--- NAdvisor.Contrib.Test/CachingAspectTest.cs
+++ NAdvisor.Contrib.Test/CachingAspectTest.cs
@@ -7,33 +7,42 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace NAdvisor.Contrib.Test
 {
     /// <summary>
-    /// Summary description for UnitTest1
+    /// Summary description for CachingAspectTest
     /// </summary>
     [TestClass]
-    public class UnitTest1
+    public class CachingAspectTest
     {
         [TestMethod]
         public void TestMethod1()
         {
+            var cc = new CachingConfig<IMethodInvokesShouldBeCachedClass>();
+            cc
+                .CreateCachingKey(
+                    cck => cck.AwfulLongCacheAbleComputation(
+                        cc.CreateKey<int>().From<string>(value => value.GetHashCode()
+                        )
+                    )
+                );
+
             var cachingAspect = new CachingAspect();
             var cachedClass = new MethodInvokesShouldBeCachedClass();
 
             var advisor = new Advisor(cachingAspect);
             var advicedProxy = advisor.GetAdvicedProxy<IMethodInvokesShouldBeCachedClass>(cachedClass);
 
-            int result = advicedProxy.AwfulLongComputation("hello");
+            int result = advicedProxy.AwfulLongCacheAbleComputation("hello");
         }
     }
 
     public interface IMethodInvokesShouldBeCachedClass
     {
-        int AwfulLongComputation(string inputParameter);
+        int AwfulLongCacheAbleComputation(string inputParameter);
     }
 
     public class MethodInvokesShouldBeCachedClass : IMethodInvokesShouldBeCachedClass
     {
 
-        public int AwfulLongComputation(string inputParameter)
+        public int AwfulLongCacheAbleComputation(string inputParameter)
         {
             int count = 0;
 
---
+++ NAdvisor.Contrib/CachingConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NAdvisor.Contrib
+{
+    public class CachingConfig<TCachedInterface>
+    {
+        public CachingKeyCreator<TCachingKey> CreateKey<TCachingKey>()
+        {
+            return new CachingKeyCreator<TCachingKey>();
+        }
+
+        public void CreateCachingKey(Action<TCachedInterface> methodToCache)
+        {
+        }
+
+        public class CachingKeyCreator<TChachingKey>
+        {
+            public TFromClass From<TFromClass>(Func<TFromClass, TChachingKey> functionToCreateKey)
+            {
+                return default(TFromClass);
+            }
+        }
+
+    }
+}
